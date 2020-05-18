623924e59394818736de91a677913ed4e0d6bb95
ShaneEverittM <shaneemurphy146@gmail.com>
2020-05-18 16:02:40 -0700

    Simpler CORS

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,52 @@
 
 extern crate rocket;
 use rocket::http::RawStr;
-
-use rocket::http::Method;
+use rocket::State;
 use rocket::{get, routes};
-use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
-
 use rocket_contrib::json::Json;
+use rocket_cors::{AllowedOrigins, Cors, CorsOptions};
+
 use serde::Serialize;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Serialize)]
 struct Greeting {
     message: String,
 }
 
+struct HitCount(AtomicUsize);
+
 #[get("/hello/<name>")]
-fn hello(name: &RawStr) -> Json<Greeting> {
+fn hello(name: &RawStr, hit_count: State<HitCount>) -> Json<Greeting> {
+    hit_count.0.fetch_add(1, Ordering::Relaxed);
     Json(Greeting {
-        message: format!("Hello, {}!", name.as_str()).into(),
+        message: format!(
+            "Hello, {}! (for the {:?}th time)",
+            name.as_str(),
+            hit_count.0
+        )
+        .into(),
     })
 }
-// Because CORS be like that
+
 fn make_cors() -> Cors {
     let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:3000"]);
     CorsOptions {
         allowed_origins,
-        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
-        allowed_headers: AllowedHeaders::some(&[
-            "Authorization",
-            "Accept",
-            "Access-Control-Allow-Origin",
-        ]),
-        allow_credentials: true,
         ..Default::default()
     }
     .to_cors()
     .expect("error while building CORS")
 }
 
-fn main() {
+fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![hello])
         .attach(make_cors())
-        .launch();
+        .manage(HitCount(AtomicUsize::new(0)))
+}
+
+fn main() {
+    rocket().launch();
 }
